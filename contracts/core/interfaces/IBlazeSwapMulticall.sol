b9dada7ab0c3694851845a7607c24030cd3c40a4
Alex Dupre <sysadmin@alexdupre.com>
2022-09-19 10:33:12 +0200

    Remove payable from multicall.

--- contracts/core/interfaces/IBlazeSwapMulticall.sol
+++ contracts/core/interfaces/IBlazeSwapMulticall.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.7.5;
 pragma abicoder v2;
 
 interface IBlazeSwapMulticall {
-    function multicall(bytes[] calldata data) external payable returns (bytes[] memory results);
+    function multicall(bytes[] calldata data) external returns (bytes[] memory results);
 }
