9a3c7a688652e8434b73682f9ca2729d78f8eb98
Alex Dupre <sysadmin@alexdupre.com>
2022-09-29 16:03:32 +0200

    Add workaround for TypeChain.

---
+++ contracts/IgnoreMe.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+/**
+ * Empty interface to work around a bug of TypeChain:
+ * https://github.com/dethcrypto/TypeChain/pull/773
+ */
+interface IgnoreMe {
+
+}
